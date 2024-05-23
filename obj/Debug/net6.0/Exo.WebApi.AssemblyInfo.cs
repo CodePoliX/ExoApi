d0b490f3320bbd92e27c14332cdd800ac8c85e03
Poliana Padilha <polianapetryy@gmail.com>
2024-05-23 15:12:55 -0300

    Finalizando configurações de segurança

--- obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ceaff89247a97cd3aa020d367d059846e52ddf23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed70ff304836bbe5e45cf725a16e6105138dc06b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
